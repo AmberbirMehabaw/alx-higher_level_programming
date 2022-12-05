1a4165ba801792c38c264895b4c929635982d0a1
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-12-05 07:49:15 -0800

    copy element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 
 
 def new_in_list(my_list, idx, element):
-    copy_list = mylist.copy()
-    if (idx < 0 or idx > (len(my_list-1)):
-        return copy_list
-    copy_list[idx] = element
-    return copy_list
+    cpy_list = my_list.copy()
+    if (idx < 0 or idx > (len(my_list) - 1)):
+        return cpy_list
+    cpy_list[idx] = element
+    return cpy_list
